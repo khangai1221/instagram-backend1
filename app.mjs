b62c7019898758fd6f58ceda3605842dc748705b
25HP7048 25HP7048 <25HP7048@G303-CC0144.local>
2025-09-26 18:32:58 +0800

    fix

--- app.mjs
+++ app.mjs
@@ -1,74 +1,140 @@
 import express from "express";
+import bcrypt from "bcrypt";
+import mongoose from "mongoose";
+import cors from "cors";
+import dotenv from "dotenv";
 
-const PORT = 3000;
+dotenv.config();
+
+const UserModel = mongoose.model("User", {
+  username: String,
+  fullname: String,
+  email: String,
+  password: String,
+  phone: String,
+});
+
+const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+const passwordRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[^A-Za-z0-9]).{8,}$/;
+const PORT = 5500;
+
 const app = express();
 
 app.use(express.json());
 
+app.use(cors());
+
 app.get("/", (req, res) => {
-  res.send("go to postman to use post to create new account")
+  res.send("Hi mom");
 });
 
-app.post("/signup", (req, res) => {
-  const body = req.body;
-
-  if (!body) {
+app.post("/signup", async (req, res) => {
+  if (!req.body) {
     return res.status(400).send({ message: "Body required" });
   }
-
+  const body = req.body;
   if (!body.credential) {
     return res.status(400).send({ message: "Credential required" });
   }
-  let credentialType;
   if (!body.password) {
     return res.status(400).send({ message: "Password required" });
   }
-
-  const password = body.password;
-
-  if (password.length < 8) {
-    return res.status(400).send({ message: "Password must be at least 8 characters long" });
-  }
-
-  if (!/[0-9]/.test(password)) {
-    return res.status(400).send({ message: "Password must include at least one number" });
-  }
-
-  if (!/[a-z]/.test(password)) {
-    return res.status(400).send({ message: "Password must include at least one lowercase letter" });
-  }
-
-  if (!/[A-Z]/.test(password)) {
-    return res.status(400).send({ message: "Password must include at least one uppercase letter" });
-  }
-
-  if (!/[!@#$%^&*()\-=+_.]/.test(password)) {
-    return res.status(400).send({ message: "Password must include at least one special character (!@#$%^&*()-=_+.)" });
-  }
-
-
   if (!body.fullname) {
     return res.status(400).send({ message: "Fullname required" });
   }
   if (!body.username) {
     return res.status(400).send({ message: "Username required" });
   }
-  if (body.credential.includes("@")) {
-    credentialType = "email";
-  } else {
-    credentialType = "phone";
+
+  let email = null;
+  let phone = null;
+
+  if (emailRegex.test(body.credential)) {
+    email = body.credential;
+  }
+  if (!isNaN(Number(body.credential))) {
+    phone = body.credential;
   }
 
-  console.log(`Credential type: ${credentialType}`);
-  console.log(`Credential value: ${body.credential}`);
-  console.log(`Password value: ${body.password}`);
-  console.log(`Username value: ${body.username}`);
-  console.log(`Fullname value: ${body.fullname}`);
+  if (!email && !phone) {
+    return res.status(400).send({ message: "Credential must be Email or Phone number!" });
+  }
 
+  if (email) {
+    const existingUserByEmail = await UserModel.findOne({ email: email });
+    if (existingUserByEmail) {
+      return res.status(400).send({ message: "User with this email already exists" });
+    }
+  }
 
-  return res.send({ message: "Welcome to Instagram" });
+  if (phone) {
+    const existingUserByPhone = await UserModel.findOne({ phone: phone });
+    if (existingUserByPhone) {
+      return res.status(400).send({ message: "User with this phone already exists" });
+    }
+  }
+
+  if (body.password.length < 8) {
+    return res.status(400).send({ message: "Password must be greater than 8 characters" });
+  }
+
+  if (!passwordRegex.test(body.password)) {
+    return res.status(400).send({ message: "Password must include Upper and Lowercase letters and digit with special characters" });
+  }
+
+  const existingUser = await UserModel.findOne({ username: body.username });
+
+  if (existingUser) {
+    return res.status(400).send({ message: `User with username "${body.username}" already exists` });
+  }
+
+  const hashedPassword = bcrypt.hashSync(body.password, 10);
+
+  const newUser = {
+    fullname: body.fullname,
+    username: body.username,
+    email,
+    phone,
+    password: hashedPassword,
+  };
+
+  const user = new UserModel(newUser);
+  await user.save();
+
+  return res.send({ message: "Welcome to instagram", body: user });
+});
+
+app.post("/signin", async (req, res) => {
+  if (!req.body) {
+    return res.status(400).send({ message: "Body required" });
+  }
+  const body = req.body;
+  if (!body.credential) {
+    return res.status(400).send({ message: "Credential required" });
+  }
+  if (!body.password) {
+    return res.status(400).send({ message: "Password required" });
+  }
+
+  const user = await UserModel.findOne({ $or: [{ email: body.credential }, { phone: body.credential }, { username: body.credential }] });
+
+  if (!user) {
+    return res.status(400).send({ message: "Wrong credentials!" });
+  }
+
+  console.log("User password:", user.password);
+  console.log("Body password:", body.password);
+
+  const isCorrectPassword = bcrypt.compareSync(body.password, user.password);
+
+  if (!isCorrectPassword) {
+    return res.status(400).send({ message: "Wrong password!" });
+  }
+
+  return res.send({ message: "You are signed in", body: user });
 });
 
 app.listen(PORT, () => {
+  mongoose.connect(process.env.MONGO_URL);
   console.log(`Your app is running on http://localhost:${PORT}`);
 });
