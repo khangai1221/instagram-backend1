6cb43ba4a3155a17d7803aa2ab987d8e6512e1b2
25HP7048 25HP7048 <25HP7048@G303-CC0144.local>
2025-10-13 16:32:18 +0800

    new

--- models/post.model.mjs
+++ models/post.model.mjs
@@ -1,14 +1,22 @@
 import mongoose from "mongoose";
-
-const PostSchema = new mongoose.Schema(
-    {
-        description: String,
-        imageUrl: String,
-        user: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
-        likes: { type: Number, default: 0 },
-        likedUsers: { type: [String], default: [] }, // store userIds
-    },
-    { timestamps: true } // auto-manage createdAt and updatedAt
+export const PostModel = mongoose.model(
+    "Post",
+    new mongoose.Schema(
+        {
+            user: { type: String, ref: "User", required: true },
+            description: { type: String, required: true },
+            imageUrl: { type: String, required: true },
+            likes: { type: Number, default: 0 },
+            likedUsers: [{ type: String, ref: "User" }],
+            comments: [
+                {
+                    userId: { type: String },
+                    user: { type: String, required: true },
+                    text: { type: String, required: true },
+                    createdAt: { type: Date, default: Date.now },
+                },
+            ],
+        },
+        { timestamps: true } // <-- move timestamps here
+    )
 );
-
-export const PostModel = mongoose.model("Post", PostSchema);
--- models/user.model.mjs
+++ models/user.model.mjs
@@ -1,11 +1,13 @@
 import mongoose from "mongoose";
+import { nanoid } from "nanoid";
 
 export const UserModel = mongoose.model(
     "User",
     new mongoose.Schema(
         {
+            _id: { type: String, default: () => nanoid() },
             fullname: { type: String, required: true },
-            username: { type: String, required: true, unique: true },
+            username: { type: String, required: true },
             email: { type: String, default: null },
             phone: { type: String, default: null },
             password: { type: String, required: true },
